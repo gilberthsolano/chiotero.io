4e18535cb2c943fa5c083ff728943ae4f398df22
gilberthsolano <49965038+gilberthsolano@users.noreply.github.com>
2024-11-11 07:41:18 -0500

    Actualización de configuración para despliegue en GitHub Pages

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,6 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/chiotero.io/' // reemplaza esto con el nombre del repositorio
-
 })
